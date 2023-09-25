8d910e224cafea6bae4813efd9c8a7b428709c60
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-09-25 14:05:54 +0600

    banner section added

---
+++ src/Components/Header/Banner/Banner.jsx
@@ -0,0 +1,41 @@
+const Banner = () => {
+  return (
+    <div className="max-w-[1600px] mx-auto font-inter">
+      <div
+        className="min-h-screen hero"
+        style={{
+          backgroundImage: "url(https://i.ibb.co/tx0F7gH/banner-img.png)",
+        }}
+      >
+        <div className="bg-white hero-overlay bg-opacity-[95%]"></div>
+        <div className="text-center hero-content text-neutral-content">
+          <div className="max-w-[780px]">
+            <h1 className="mb-5 md:text-[32px] text-[20px] lg:text-[48px]  font-bold text-[#0B0B0B]">
+              I Grow By Helping People In Need
+            </h1>
+            <div className="hidden join md:block">
+              <input
+                className="text-gray-700 input input-bordered join-item"
+                placeholder="Search here..."
+              />
+              <button className="rounded-xl btn join-item bg-[#FF444A] hover:bg-red-300 text-[16px] font-semibold text-white normal-case">
+                Search
+              </button>
+            </div>
+            <div className="flex flex-col justify-center items-center gap-[16px] md:hidden">
+              <input
+                className="text-gray-700 input input-bordered w-[90%]"
+                placeholder="Search here..."
+              />
+              <button className="rounded-lg btn bg-[#FF444A] hover:bg-red-300 text-[16px] font-semibold text-white normal-case">
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
