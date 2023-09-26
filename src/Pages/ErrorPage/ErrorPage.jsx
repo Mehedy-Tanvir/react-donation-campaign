9e06c853341a0309f709a60db865002375483181
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-09-26 13:30:08 +0600

    error page updated

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -4,7 +4,9 @@ const ErrorPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-6">
       <img src="/Logo.png" alt="" />
-      <h1 className="text-4xl font-bold">Page not found!!!!</h1>
+      <h1 className="text-4xl font-bold text-center text-[#0B0B0BB3]">
+        Error 404! Page not found!
+      </h1>
       <Link to="/">
         <button className="text-2xl font-semibold text-white normal-case bg-red-600 rounded-lg btn hover:bg-red-500 px-[24px] h-[60px]">
           Go Home
