a29cb7f52e32b80e5356559fc5e385532bfeb2aa
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-09-26 11:49:48 +0600

    error page created

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen gap-6">
+      <img src="/Logo.png" alt="" />
       <h1 className="text-4xl font-bold">Page not found!!!!</h1>
+      <Link to="/">
+        <button className="text-2xl font-semibold text-white normal-case bg-red-600 rounded-lg btn hover:bg-red-500 px-[24px] h-[60px]">
+          Go Home
+        </button>
+      </Link>
     </div>
   );
 };
